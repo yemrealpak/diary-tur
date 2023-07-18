af4e7e5b0d8cddb3d0b0829b3babe395fce9c19b
GitKodland <125306926+GitKodland@users.noreply.github.com>
2023-07-18 15:22:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
-# Import
+# İçe aktar
 from flask import Flask, render_template,request, redirect
-# Connecting the database library
+# Veri tabanı kitaplığını bağlama
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-# Connecting SQLite
+# SQLite'ı bağlama
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diary.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# Creating a DB
+# Veri tabanı oluşturma
 db = SQLAlchemy(app)
-# Creating a table
+# Tablo oluşturma
 
 class Card(db.Model):
-    # Creating columns
+    # Sütun oluşturma
     # id
     id = db.Column(db.Integer, primary_key=True)
-    # Title
+    # Başlık
     title = db.Column(db.String(100), nullable=False)
-    # Description
+    # Tanım
     subtitle = db.Column(db.String(300), nullable=False)
-    # Text
+    # Metin
     text = db.Column(db.Text, nullable=False)
 
-    # Outputting the object and id
+    # Nesnenin ve kimliğin çıktısı
     def __repr__(self):
         return f'<Card {self.id}>'
     
 
-#Assignment #2. Create the User table
+#Ödev #2. Kullanıcı tablosunu oluşturun
 
 
 
@@ -38,7 +38,7 @@ class Card(db.Model):
 
 
 
-# Running the content page
+# İçerik sayfasını çalıştırma
 @app.route('/', methods=['GET','POST'])
 def login():
         error = ''
@@ -46,7 +46,7 @@ def login():
             form_login = request.form['email']
             form_password = request.form['password']
             
-            #Assignment #4. Implement authorisation
+            #Ödev #4. yetkilendirmeyi uygulamak
             
 
 
@@ -62,7 +62,7 @@ def reg():
         login= request.form['email']
         password = request.form['password']
         
-        #Assignment #3. Make it so that the user's data is recorded to the database
+        #Ödev #3 Kullanıcı verilerinin veri tabanına kaydedilmesini sağlayın
         
 
         
@@ -72,26 +72,26 @@ def reg():
         return render_template('registration.html')
 
 
-# Running the content page
+# İçerik sayfasını çalıştırma
 @app.route('/index')
 def index():
-    # Displaying the databaase entries
+    # Veri tabanı girişlerini görüntüleme
     cards = Card.query.order_by(Card.id).all()
     return render_template('index.html', cards=cards)
 
-# Running the page with the entry
+# Kayıt sayfasını çalıştırma
 @app.route('/card/<int:id>')
 def card(id):
     card = Card.query.get(id)
 
     return render_template('card.html', card=card)
 
-# Running the entry creation page
+# Giriş oluşturma sayfasını çalıştırma
 @app.route('/create')
 def create():
     return render_template('create_card.html')
 
-# The entry forn
+# Giriş formu
 @app.route('/form_create', methods=['GET','POST'])
 def form_create():
     if request.method == 'POST':
@@ -99,7 +99,7 @@ def form_create():
         subtitle =  request.form['subtitle']
         text =  request.form['text']
 
-        # Creating an object that will be sent to the DB
+        # Veri tabanına gönderilecek bir nesne oluşturma
         card = Card(title=title, subtitle=subtitle, text=text)
 
         db.session.add(card)
